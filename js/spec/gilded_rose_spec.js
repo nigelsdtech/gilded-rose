17e073affeee5f2d1d8d0719cd2e7c5c5b0bd845
Nigel Dsouza <stephen.dsouza@openbet.com>
2019-06-23 20:15:56 +0100

    Getting to grips with the jasmine testing suite. Added a basic test to confirm existing code.

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,14 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
 
+  describe("Aged Brie", function() {
+    it("should increase in quality by one in one day", function() {
+      items = [ new Item("Aged Brie", 10, 1) ];
+      update_quality();
+      expect(items[0].name).toEqual("Aged Brie");
+      expect(items[0].quality).toEqual(2);
+      expect(items[0].sell_in).toEqual(9);
+    });
+
+  });
 });
